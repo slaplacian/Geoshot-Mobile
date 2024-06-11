c75eb941c8fdd7cdebe2e7d81ee73472bbb3b46c
Leo Victor Cruz Vasconcelos <leotpw9@gmail.com>
2024-06-11 16:46:10 -0300

    Iniciar Search

--- app/src/main/java/com/example/geoshot/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/geoshot/ui/search/SearchFragment.java
@@ -1,9 +1,13 @@
 package com.example.geoshot.ui.search;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +16,26 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.geoshot.R;
 import com.example.geoshot.databinding.FragmentSearchBinding;
+import com.example.geoshot.utils.sqlite.SessionManager;
 
 public class SearchFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    ImageView userPhotoView;
+    Button searchButton, toggleButton;
+    EditText searchEditText;
+    TextView usernameText;
 
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        String loggedUser = SessionManager.getSession(this.getContext());
+    }
+
 }
