e720b73e99837508f3934f4802ba02efc2605acf
HamiltonMussi <hamiltonmussi.cf@gmail.com>
2024-06-11 21:33:14 -0300

    navigation updates

--- app/src/main/java/com/example/geoshot/BaseActivity.java
+++ app/src/main/java/com/example/geoshot/BaseActivity.java
@@ -2,25 +2,15 @@ package com.example.geoshot;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
 
-import com.example.geoshot.ui.createChallenge.CreateChallengeFragment;
-import com.example.geoshot.ui.home.HomeFragment;
-import com.example.geoshot.ui.perfil.PerfilFragment;
-import com.example.geoshot.ui.search.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 public class BaseActivity extends AppCompatActivity {
-    private final HomeFragment homeFragment = new HomeFragment();
-    private final SearchFragment searchFragment = new SearchFragment();
-    private final CreateChallengeFragment createChallengeFragment = new CreateChallengeFragment();
-    private final PerfilFragment perfilFragment = new PerfilFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,50 +20,11 @@ public class BaseActivity extends AppCompatActivity {
         String loggedUsername = getIntent().getStringExtra("username");
         Log.d("Depurando", "BaseActivity -> onCreate -> LoggedUser -> " + loggedUsername);
 
-        Bundle bundle = new Bundle();
-        bundle.putString("username", loggedUsername);
-        homeFragment.setArguments(bundle);
-        searchFragment.setArguments(bundle);
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        loadFragment(homeFragment, true);
-        Log.d("Depurando", "BaseActivity -> onCreate -> Navegando para home...");
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.container);
+        NavController navController = navHostFragment.getNavController();
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-
-                if (itemId == R.id.search) {
-                    loadFragment(searchFragment, false);
-                }
-                else if (itemId == R.id.createChallenge) {
-                    loadFragment(createChallengeFragment, false);
-                }
-                else if (itemId == R.id.perfil) {
-                    loadFragment(perfilFragment, false);
-                }
-                else{
-                    loadFragment(homeFragment, false);
-                }
-
-                return true;
-            }
-        });
-    }
-
-    private void loadFragment(Fragment fragment, boolean isAppNotInitialized) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-
-        if(isAppNotInitialized){
-            fragmentTransaction.add(R.id.container, fragment);
-        }
-        else{
-            fragmentTransaction.replace(R.id.container, fragment);
-        }
-
-//        fragmentTransaction.replace(R.id.container, fragment);
-        fragmentTransaction.commit();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
--- app/src/main/java/com/example/geoshot/ui/perfil/PerfilFragment.java
+++ app/src/main/java/com/example/geoshot/ui/perfil/PerfilFragment.java
@@ -7,19 +7,28 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.geoshot.R;
 
 public class PerfilFragment extends Fragment {
-
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        Button editProfileButton = view.findViewById(R.id.edit_profile_button);
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(getActivity(), R.id.container)
+                        .navigate(R.id.action_perfil_to_editProfile);
+            }
+        });
+        return view;
     }
-
 }
